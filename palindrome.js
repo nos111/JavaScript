64b0be861e5dfa079565660c8f60bb20b158f9b8
nos111 <nsaffour@gmail.com>
2017-05-08 11:15:46 +0200

    Palindrome in Javascript

---
+++ palindrome.js
@@ -0,0 +1,46 @@
+function palindrome(str) {
+var string = str;
+string = string.toLowerCase();
+var straightArray = string;
+var reversedArray = string;
+straightArray = straightArray.split("");
+reversedArray = reversedArray.split("");
+reversedArray = reversedArray.reverse();
+function checkCharacters (array) {
+    for (var i = 0; i < array.length; i++) {
+    var testArray = [" ","_","-",",",".","*","#",")","$","@","(","\\","/"];
+        for (var x = 0; x < testArray.length; x++) {
+            if (array[i] === testArray[x]) {
+                delete array[i];
+            }
+        }
+    }    
+}
+checkCharacters(straightArray);
+checkCharacters(reversedArray);
+console.log(straightArray);
+console.log(reversedArray);
+straightArray = straightArray.join("");
+straightArray = straightArray.split("");
+reversedArray = reversedArray.join("");
+reversedArray = reversedArray.split("");
+function arraysEqual(arr1, arr2) {
+  for (var i = 0, len = arr1.length; i < len; i++){
+    if (arr1[i] !== arr2[i]){
+      console.log(false);
+      return false;
+        }
+    }
+  console.log(true);
+  return true;
+  }
+return arraysEqual(straightArray, reversedArray);
+}
+
+palindrome("A man, a plan, a canal. Panama");
+
+
+
+
+
+
